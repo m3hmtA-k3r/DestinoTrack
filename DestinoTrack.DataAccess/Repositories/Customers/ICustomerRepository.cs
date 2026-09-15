94987f6c7844f97ef2d10ce336302c2b593f68b5
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2026-09-15 02:07:30 +0200

    VAr olan Test

--- DestinoTrack.DataAccess/Repositories/Customers/ICustomerRepository.cs
+++ DestinoTrack.DataAccess/Repositories/Customers/ICustomerRepository.cs
@@ -8,6 +8,8 @@ namespace DestinoTrack.DataAccess.Repositories.Customers
         // aynı ülkede aynı kimlik / vergi numarasıyla ikinci müşteri açılamaz
         Task<bool> TaxNumberExistsAsync(Guid countryId, string taxNumber);
 
+
+
         // Otomatik üretilen müşteri kodu daha önce kullanılmış mı (Code tekil)
         Task<bool> CodeExistsAsync(string code);
     }
